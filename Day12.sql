5e716cba0efaa0d365fd7bd87f49df3f361fbafc
mjcccccc <markjames.cautibar@gmail.com>
2024-10-30 00:15:18 +0800

    Day12

---
+++ Day12.sql
@@ -0,0 +1,88 @@
+/*
+	Day 12
+	Oct 29, 2024
+*/
+
+USE SQLChallenge;
+
+-- Create Employees table
+DROP TABLE IF EXISTS Employees;
+CREATE TABLE Employees (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+-- Insert sample data into Employees table
+INSERT INTO Employees (id, name) VALUES
+    (1, 'Alice'),
+    (7, 'Bob'),
+    (11, 'Meir'),
+    (90, 'Winston'),
+    (3, 'Jonathan');
+
+
+-- Create EmployeeUNI table
+DROP TABLE IF EXISTS EmployeeUNI;
+CREATE TABLE EmployeeUNI (
+    id INT PRIMARY KEY,
+    unique_id INT
+);
+
+-- Insert sample data into EmployeeUNI table
+INSERT INTO EmployeeUNI (id, unique_id) VALUES
+    (3, 1),
+    (11, 2),
+    (90, 3);
+
+/*
+
+Question: Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+
+Return employee name and their unique_id.
+
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+*/
+
+SELECT * FROM Employees
+SELECT * FROM EmployeeUNI
+
+SELECT
+	em.name,
+	eu.unique_id
+FROM Employees AS em
+LEFT JOIN EmployeeUNI AS eu
+	ON em.id = eu.id
+ORDER BY unique_id
+
+
+/*
+Question: Your task to replace null values to 0 for the employee who doesn't have unique id
+*/
+SELECT 
+	em.name,
+	COALESCE(eu.unique_id, 0)
+FROM Employees AS em
+LEFT JOIN EmployeeUNI AS eu
+	ON em.id = eu.id
